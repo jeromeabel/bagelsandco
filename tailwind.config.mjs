d173f5f566908d6300ec28348fc7bfd77b935c32
Jérôme Abel <contact@jeromeabel.net>
2023-12-12 14:28:50 +0100

    setup: Astro Projeect

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				black: '#151515',
+				white: '#fdfdfd',
+			},
+			container: {
+				center: true,
+			},
+		}
+	},
+	plugins: [],
+}
