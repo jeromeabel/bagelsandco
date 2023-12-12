457bcb9657243b5015f20b393683da2f4adc0689
Jérôme Abel <contact@jeromeabel.net>
2023-12-12 14:53:31 +0100

    css: add colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,8 +4,12 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				black: '#151515',
-				white: '#fdfdfd',
+				dark: '#97CB29',
+				darker: '#1A1A1A',
+				light: '#FFFFFF',
+				gris: '#C39F65',
+				"gris-light": '#F4F4F4',
+				purple: '#382213',
 			},
 			container: {
 				center: true,
